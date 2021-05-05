cd4d7795506d40431eed5fe61881a0ee4a0a0496
Mandeep Singh <mandeep.99@yahoo.com>
2021-05-05 17:54:53 +0530

    done with add complaint

--- app/src/main/java/co/mandeep_singh/vitcomplaint/FriendsFragement.java
+++ app/src/main/java/co/mandeep_singh/vitcomplaint/FriendsFragement.java
@@ -1,10 +1,12 @@
 package co.mandeep_singh.vitcomplaint;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -14,7 +16,8 @@ import androidx.fragment.app.Fragment;
 
 public class FriendsFragement extends Fragment {
     ListView list;
-
+    ImageButton friendsListBtn;
+    View rootView;
     String[] maintitle ={
             "Title 1","Title 2",
             "Title 3","Title 4",
@@ -35,12 +38,23 @@ public class FriendsFragement extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_friends, container,false);
+        rootView = inflater.inflate(R.layout.fragment_friends, container,false);
         MyListAdapter2 adapter=new MyListAdapter2(getActivity(), maintitle, subtitle,imgid);
+        friendsListBtn = rootView.findViewById(R.id.friends_btn);
         list=(ListView)rootView.findViewById(R.id.list_friends);
         list.setAdapter(adapter);
         establishList();
+        establishFriendsListBtn();
         return rootView;
+    }
+
+    private void establishFriendsListBtn() {
+        friendsListBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     private void establishList() {
@@ -54,4 +68,6 @@ public class FriendsFragement extends Fragment {
             }
         });
     }
+
+
 }
--- app/src/main/java/co/mandeep_singh/vitcomplaint/HomeFragement.java
+++ app/src/main/java/co/mandeep_singh/vitcomplaint/HomeFragement.java
@@ -5,16 +5,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
 public class HomeFragement extends Fragment {
     ListView list;
-
+    ImageButton plusButton;
+    View rootView;
     String[] maintitle ={
             "Title 1","Title 2",
             "Title 3","Title 4",
@@ -35,12 +42,50 @@ public class HomeFragement extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_home, container,false);
+        rootView = inflater.inflate(R.layout.fragment_home, container,false);
         MyListAdapter adapter=new MyListAdapter(getActivity(), maintitle, subtitle,imgid);
         list=(ListView)rootView.findViewById(R.id.list);
+        plusButton = (ImageButton)rootView.findViewById(R.id.plusButton);
         list.setAdapter(adapter);
         establishList();
+        establishAddComplaint();
         return rootView;
+    }
+
+    private void establishAddComplaint() {
+
+
+        plusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(getActivity(), R.style.BottomSheetDialogTheme);
+                View bottomSheetView = LayoutInflater.from(getActivity()).inflate(R.layout.layout_bottom_sheet,(LinearLayout)rootView.findViewById(R.id.bottomSheetContainer));
+                Spinner spinner1 = (Spinner) bottomSheetView.findViewById(R.id.complaints_type);
+                ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(rootView.getContext(),
+                        R.array.complaint_type, android.R.layout.simple_spinner_item);
+                adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner1.setAdapter(adapter1);
+                Spinner spinner2 = (Spinner) bottomSheetView.findViewById(R.id.status_type2);
+                ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(rootView.getContext(),
+                        R.array.status_array, android.R.layout.simple_spinner_item);
+                adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner2.setAdapter(adapter2);
+                Spinner spinner3 = (Spinner) bottomSheetView.findViewById(R.id.urgent_type2);
+                ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(rootView.getContext(),
+                        R.array.urgent_array, android.R.layout.simple_spinner_item);
+                adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner3.setAdapter(adapter3);
+
+                bottomSheetView.findViewById(R.id.submit_complaint).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        bottomSheetDialog.dismiss();
+                    }
+                });
+                bottomSheetDialog.setContentView(bottomSheetView);
+                bottomSheetDialog.show();
+            }
+        });
     }
 
     private void establishList() {
